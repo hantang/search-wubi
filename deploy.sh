c9aeca20cbbeeae5a38424dca95456740d0817f3
hantang <34468720+hantang@users.noreply.github.com>
2024-10-15 09:07:46 +0800

    Update

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,16 @@ set -eu
 TARGET="${1:-site}"
 SOURCE="${2:-web}"
 
+echo "install deps"
+pip install -r requirements.txt >/dev/null 2>&1
+
 if [[ -d $TARGET ]]; then
+    echo "clean $TARGET"
     rm -rf $TARGET
 fi
 
-python scripts/parse.py data $SOURCE/data
-cp -r $SOURCE $TARGET/
+echo "Update data"
+python scripts/parse.py data $SOURCE/data >/dev/null 2>&1
+cp -r $SOURCE $TARGET
 
 echo "Done"
